07b89587a6ea20fd9ae2d0702b980336e3eb09d8
Just-AgA <161239277+Just-AgA@users.noreply.github.com>
2025-11-11 21:44:09 +0100

    Switch from vanilla node.js to express framework,start with the static files

--- index.js
+++ index.js
@@ -1,56 +1,60 @@
 const http = require('node:http');
 const path = require('node:path');
 const fs = require('node:fs');
+const express = require('express');
+const app = express();
 
 const PORT = 8080;
 
-const server = http.createServer((req, res) => {
-  let filepath;
+app.use(express.static(__dirname));
 
-  if (req.url === '/style.css') {
-    const cssPath = path.join(__dirname, 'style.css');
+// const server = http.createServer((req, res) => {
+//   let filepath;
 
-    fs.readFile(cssPath, (err, data) => {
-      if (err) {
-        res.writeHead(500);
-        res.end('Error loading CSS');
-      } else {
-        res.writeHead(200, { 'Content-Type': 'text/css' });
-        res.end(data);
-      }
-    });
-    return;
-  }
+//   if (req.url === '/style.css') {
+//     const cssPath = path.join(__dirname, 'style.css');
 
-  switch (req.url) {
-    case '/':
-      filepath = path.join(__dirname, 'index.html');
-      break;
-    case '/home':
-      res.writeHead(302, { Location: '/' });
-      res.end();
-      return;
-    case '/about':
-      filepath = path.join(__dirname, 'about.html');
-      break;
-    case '/contact-me':
-      filepath = path.join(__dirname, 'contact-me.html');
-      break;
-    default:
-      filepath = path.join(__dirname, '404.html');
-  }
+//     fs.readFile(cssPath, (err, data) => {
+//       if (err) {
+//         res.writeHead(500);
+//         res.end('Error loading CSS');
+//       } else {
+//         res.writeHead(200, { 'Content-Type': 'text/css' });
+//         res.end(data);
+//       }
+//     });
+//     return;
+//   }
 
-  fs.readFile(filepath, (err, data) => {
-    if (err) {
-      res.writeHead(500, { 'Content-Type': 'text/plain' });
-      res.end('500 - Internal Server Error');
-    } else {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(data);
-    }
-  });
-});
+//   switch (req.url) {
+//     case '/':
+//       filepath = path.join(__dirname, 'index.html');
+//       break;
+//     case '/home':
+//       res.writeHead(302, { Location: '/' });
+//       res.end();
+//       return;
+//     case '/about':
+//       filepath = path.join(__dirname, 'about.html');
+//       break;
+//     case '/contact-me':
+//       filepath = path.join(__dirname, 'contact-me.html');
+//       break;
+//     default:
+//       filepath = path.join(__dirname, '404.html');
+//   }
 
-server.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
+//   fs.readFile(filepath, (err, data) => {
+//     if (err) {
+//       res.writeHead(500, { 'Content-Type': 'text/plain' });
+//       res.end('500 - Internal Server Error');
+//     } else {
+//       res.writeHead(200, { 'Content-Type': 'text/html' });
+//       res.end(data);
+//     }
+//   });
+// });
+
+// server.listen(PORT, () => {
+//   console.log(`Server is listening on port ${PORT}`);
+// });
